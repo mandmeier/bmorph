d9bbe6b7c5cedf5736032a24d88a344ed6a2325a
mmeier <michael.meier@huskers.unl.edu>
2020-10-22 10:57:42 -0500

    game buttons

--- biomorph.py
+++ biomorph.py
@@ -12,9 +12,6 @@ white, black, purple, blue = (
 
 
 height, width = 1000, 1000
-pygame.init()
-pygame.display.set_caption("Fractal Tree")
-screen = pygame.display.set_mode((height, width))
 
 # grid locations
 shift = height / 12
@@ -28,6 +25,9 @@ pos7 = (width / 6, 5 * height / 6 + shift)
 pos8 = (width / 2, 5 * height / 6 + shift)
 pos9 = (5 * width / 6, 5 * height / 6 + shift)
 
+btn_positions = [pos1,pos2,pos3,pos4,pos6,pos7,pos8,pos9]
+
+
 
 def FractalTree(branchings, angle, stem_length, shrink_factor, squeeze_x, squeeze_y, start_pos, direction=-math.pi / 2, color=black):
     # default direction means Y pointing up
@@ -35,7 +35,7 @@ def FractalTree(branchings, angle, stem_length, shrink_factor, squeeze_x, squeez
     angle_y = stem_length * math.sin(direction)
     (x, y) = start_pos
     next_position = (x + angle_x * squeeze_x, y + angle_y * squeeze_y)
-    pygame.draw.line(screen, color, start_pos, next_position, 5)
+    pygame.draw.line(screen, color, start_pos, next_position, 2)
 
     if branchings > 0:
         new = stem_length * shrink_factor
@@ -109,8 +109,122 @@ class Biomorph:
                     squeeze_x, squeeze_y, start_pos=start_pos)
 
 
+
+# for ch in children:
+# print(ch.genome())
+
+
+class Button():
+    def __init__(self, color, x, y, width, height, child, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.child = child
+
+    def draw(self, win, outline=None):
+        # Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(win, outline, (self.x - 2, self.y -
+                                            2, self.width + 4, self.height + 4), 0)
+
+        pygame.draw.rect(win, self.color, (self.x, self.y,
+                                           self.width, self.height), 0)
+
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 30)
+            text = font.render(self.text, 1, (0, 0, 0))
+            win.blit(text, (self.x + (self.width / 2 - text.get_width() / 2),
+                            self.y + (self.height / 2 - text.get_height() / 2)))
+
+    def isOver(self, pos):
+        # Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+
+        return False
+
+
+def display_gen(screen, parent):
+
+    children = parent.create_children()
+
+    buttons = [
+        Button((0, 255, 0), pos1[0] - 75, pos1[1] +
+            20, 150, 50, children[0], 'child 1'),
+        Button((0, 255, 0), pos2[0] - 75, pos2[1] +
+            20, 150, 50, children[1], 'child 2'),
+        Button((0, 255, 0), pos3[0] - 75, pos3[1] +
+            20, 150, 50, children[2], 'child 3'),
+        Button((0, 255, 0), pos4[0] - 75, pos4[1] +
+            20, 150, 50, children[3], 'child 4'),
+        Button((0, 255, 0), pos6[0] - 75, pos6[1] +
+            20, 150, 50, children[4], 'child 5'),
+        Button((0, 255, 0), pos7[0] - 75, pos7[1] +
+            20, 150, 50, children[5], 'child 6'),
+        Button((0, 255, 0), pos8[0] - 75, pos8[1] +
+            20, 150, 50, children[6], 'child 7'),
+        Button((0, 255, 0), pos9[0] - 75, pos9[1] +
+            20, 150, 50, children[7], 'child 8'),
+    ]
+
+    generation = len(parent.ancestors) - 1
+    text = txtfont.render(f'generation {generation}', True, purple, white)
+    textRect = text.get_rect()
+    # set the center of the rectangular object.
+    textRect.center = (500, 20)
+
+    running = True
+    while running:
+
+        screen.fill(white)
+
+        # draw parent
+        parent.draw(start_pos=pos5)
+
+        # draw children
+        for child, btnpos in zip(children, btn_positions):
+            child.draw(start_pos=btnpos)
+
+        # draw buttons
+        for button in buttons:
+            button.draw(screen, (0, 0, 0))
+
+        screen.blit(text, textRect)
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
+
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for button in buttons:
+                    if button.isOver(pos):
+                        print(f'selected {button.text}')
+                        display_gen(screen, button.child)
+                        pygame.quit()
+
+            if event.type == pygame.MOUSEMOTION:
+                for button in buttons:
+                    if button.isOver(pos):
+                        button.color = (255, 0, 0)
+                    else:
+                        button.color = (0, 255, 0)
+
+
+pygame.init()
+pygame.display.set_caption("Fractal Tree")
+screen = pygame.display.set_mode((height, width))
+
+
 parent = Biomorph(
-    branchings=3,
+    branchings=0,
     angle=math.radians(30),
     stem_length=50,
     shrink_factor=0.8,
@@ -118,29 +232,6 @@ parent = Biomorph(
     squeeze_y=1,
 )
 
+txtfont = pygame.font.Font('freesansbold.ttf', 20)
 
-children = parent.create_children()
-
-#for ch in children:
-    #print(ch.genome())
-
-running = True
-while running:
-
-    screen.fill(white)
-
-    children[0].draw(start_pos=pos1)
-    children[1].draw(start_pos=pos2)
-    children[2].draw(start_pos=pos3)
-    children[3].draw(start_pos=pos4)
-    parent.draw(start_pos=pos5)
-    children[4].draw(start_pos=pos6)
-    children[5].draw(start_pos=pos7)
-    children[6].draw(start_pos=pos8)
-    children[7].draw(start_pos=pos9)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    pygame.display.update()
+display_gen(screen, parent)
